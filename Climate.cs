50ec4ebf0316c042e30a3043a5b3f58ebccc45c3
Ray Jackson <roguej2@gmail.com>
2019-09-29 02:53:09 -0400

    Time passes, changing the seasons

--- Climate.cs
+++ Climate.cs
@@ -1,27 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Climate : MonoBehavior
+public class Climate : MonoBehaviour
 {
 	[System.Serializable]
 	public class Season
 	{
 		public int numberDays;
-		public String name;
-		public Weather[] possibleWeather;
+		public string name;
+		//array of particle effects for the weather
 	}
 	
 	public Season[] possibleSeasons;
-	public int currentSeason;
-	
-	void setSeason(int index)
+	public Season currentSeason;
+	public int index = 0;
+	private int currentDay = 0;
+
+    [Tooltip("Need to manually set this in the editor")]
+    public Text seasonDisplay;
+
+    private void Start()
+    {
+        currentSeason = possibleSeasons[index];
+        displaySeason();
+    }
+
+    void incrementSeason()
 	{
-		currentSeason = index;
-	}
+		index++;
+		if(index >= possibleSeasons.Length)
+		{
+			index = 0;
+		}
+		currentSeason = possibleSeasons[index];
+        displaySeason();
+
+    }
+
 	
-	void displaySeason(Season season)
+	public void incrementDay()
 	{
-		
+		currentDay++;
+		if(currentDay > currentSeason.numberDays)
+		{
+			incrementSeason();
+			currentDay = 0;
+            displaySeason();
+
+        }
 	}
+
+    void displaySeason()
+    {
+        seasonDisplay.text = string.Format("{0} Day {1}",currentSeason.name, currentDay);
+    }
 }
---
+++ TimeKeeper.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeKeeper : MonoBehaviour
+{
+	[Range(0f,60f)]
+	[Tooltip("How long, in minutes, each day should last. Max length is one hour.")]
+    public float dayLength = 5.0f;
+	
+	[Tooltip("Increments at each sunrise")]
+    public int currentDay = 0;
+		
+	public int yearLength = 30; //Homage to digimon world :)
+
+	[Tooltip("Increments after the year length has ended")]
+    public int currentYear = 0;
+	
+	[Range(0f, 1f)]
+	public float currentTime;
+	
+	public bool pause = false;
+	private Climate climate;
+
+    [Tooltip("Need to manually set this in the editor")]
+    public Text yearDisplay;
+	
+	void Start()
+	{
+        climate = GetComponentInParent<Climate>();
+        displayDay();
+
+    }
+
+    void Update()
+    {
+		if(!pause)
+		{
+			UpdateTime();
+		}     
+        
+    }
+	
+	void UpdateTime()
+	{
+		if(dayLength > 0)
+		{
+			float scale = 24 / (dayLength / 60); //scale real time to game time
+			currentTime += Time.deltaTime * scale / 86400;
+			
+			float sunAngle = currentTime * 360f;
+			transform.localRotation = Quaternion.Euler(new Vector3(sunAngle, 0));
+			
+			if(currentTime > 1)
+			{
+				currentDay++;
+                displayDay();
+                if (currentDay > yearLength)
+                {
+                    currentYear++;
+                    currentDay = 0;
+                    displayDay();
+                }
+				currentTime = 0;
+                climate.incrementDay();
+			}
+		}
+		else
+		{
+			pause = true;
+		}
+	}
+
+    void displayDay()
+    {
+
+        yearDisplay.text = string.Format("Year {0} Day {1}", currentYear, currentDay);
+    }
+
+
+}
